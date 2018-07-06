402e7d67c9535a22d031f23cf0eb9034d7c2c027
Timothy Morrise <timothy.morrise@gmail.com>
2018-07-06 17:07:09 -0600

    finished styling and footer

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const newMarquee = document.getElementById("new-marquee")
 const form = document.getElementById("form")
 const button = document.getElementById("button")
 const letterZone = document.getElementById("letter-zone")
+const wordZone = document.getElementById("word-zone")
 
 // VARIABLE FOR CODE
 let buttonStatus = "hasNotSaladYet"
@@ -41,29 +42,34 @@ let defaultLetters = {
     Z: 0
 }
 
-let letters = {...defaultLetters}
+let letters = { ...defaultLetters }
 
 // BUTTON FUNCTIONS
 
 button.onclick = function (e) {
     e.preventDefault();
-    letterZone.innerHTML = ""
-    let text1 = oldMarquee.value.toUpperCase().replace(/[^A-Z]/g, '');
-    let text2 = newMarquee.value.toUpperCase().replace(/[^A-Z]/g, '');
-    for (let i = 0; i < text2.length; i++) {
-        letters[text2[i]]++
-    }
-    for (i = 0; i < text1.length; i++) {
-        letters[text1[i]]--
-    }
-    let insertTags = ""
-    for (var key in letters) {
-        if (letters[key] > 0) {
-            insertTags += `<h1>${key} : ${String(letters[key])}</h1>`
+    if (oldMarquee.value && newMarquee.value) {
+        letterZone.innerHTML = ""
+        let text1 = oldMarquee.value.toUpperCase().replace(/[^A-Z]/g, '');
+        let text2 = newMarquee.value.toUpperCase().replace(/[^A-Z]/g, '');
+        for (let i = 0; i < text2.length; i++) {
+            letters[text2[i]]++
         }
+        for (i = 0; i < text1.length; i++) {
+            letters[text1[i]]--
+        }
+        wordZone.innerHTML = `<h3>${oldMarquee.value.toUpperCase()}</h3>
+        <h3>&darr;</h3>
+        <h3>${newMarquee.value.toUpperCase()}</h3>`
+        let insertTags = ""
+        for (var key in letters) {
+            if (letters[key] > 0) {
+                insertTags += `<h1>${key} : ${String(letters[key])}</h1>`
+            }
+        }
+        letterZone.innerHTML = insertTags
+        oldMarquee.value = ""
+        newMarquee.value = ""
+        letters = { ...defaultLetters }
     }
-    letterZone.innerHTML = insertTags
-    oldMarquee.value = ""
-    newMarquee.value = ""
-    letters = {...defaultLetters}
 }
